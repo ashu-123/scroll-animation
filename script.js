a2c5f674c7a54ec6ed4327e727e8130e3fbdc79d
Ashutosh <Ashutosh>
2023-12-30 13:43:59 +0530

    Added script to listen to scroll events

---
+++ script.js
@@ -0,0 +1,18 @@
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', manageBoxes);
+
+manageBoxes();
+
+function manageBoxes() {
+    const windowTop = window.innerHeight/5*4;
+
+    boxes.forEach(box => {
+        if(box.getBoundingClientRect().top<windowTop) {
+            box.classList.add('show');
+        }
+        else {
+            box.classList.remove('show');
+        }
+    })
+}
